14307aece13f4be994d2ef0fcf4fb03a47817650
Munteanu Lucian <lucian_munteanu@mail.ru>
2026-01-14 21:54:42 +0200

    Update content

--- static/invites/VI_ConfirmationFormModel.js
+++ static/invites/VI_ConfirmationFormModel.js
@@ -152,12 +152,7 @@ function CustomizeConfirmationFormModel() {
                 { value: 2, text: "2" },
                 { value: 3, text: "3" },
                 { value: 4, text: "4" },
-                { value: 5, text: "5" },
-                { value: 6, text: "6" },
-                { value: 7, text: "7" },
-                { value: 8, text: "8" },
-                { value: 9, text: "9" },
-                { value: 10, text: "10" }
+                { value: 5, text: "5" }
             ],
             
         },
